9ae78dbafd6014e1dda3d12d9513afdd0eb0ae56
Siddharth1605 <Siddhashokan@gmail.com>
2023-11-01 21:12:37 +0530

    Minimum jumps

---
+++ GreedyAlgorithm/7MinimumJump.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean canJump(int[] nums) {
+       int reachable = 0;
+       for(int i = 0; i < nums.length; i ++) {
+           if(i > reachable) return false;
+           reachable = Math.max(reachable, i + nums[i]);
+       } 
+       return true;
+    }
+} {
+    
+}
