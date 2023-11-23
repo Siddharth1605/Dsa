d4ef51af66bb8f0b5c576cd54d7ea992046497fa
Siddharth1605 <Siddhashokan@gmail.com>
2023-11-23 10:12:34 +0530

    Stack Span Problem

---
+++ StackAndQueue/35StockSpan.java
@@ -0,0 +1,19 @@
+class StockSpanner {
+    Stack<int []> st;
+    public StockSpanner() {
+        st = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int span = 1;
+
+        while(!st.isEmpty() && price >= st.peek()[0])
+            span += st.pop()[1];
+
+        st.push(new int[] {price, span});
+
+        return span;
+    }
+}
+
+ 
