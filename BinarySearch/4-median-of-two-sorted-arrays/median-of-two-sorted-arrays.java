4b3daab18e3ea2f7605c1495e3dbc4080813b6fc
Siddharth1605 <110531782+Siddharth1605@users.noreply.github.com>
2024-02-03 22:00:18 +0530

    Time: 1 ms (100.00%) | Memory: 46 MB (35.33%) - LeetSync

--- BinarySearch/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ BinarySearch/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         //Better Approach Tc:O(n)
@@ -54,5 +55,50 @@ class Solution {
         if(n%2 == 1)
             return i2e2;
         return (double)((double)(i1e1 + i2e2)) / 2.0;
+    }
+}
+*/
+//Optimized solution O(log(min(n1, n2)))
+class Solution {
+    public double findMedianSortedArrays(int[] arr1, int[] arr2) {
+        if(arr1.length > arr2.length)
+            return median(arr2, arr1);
+        return median(arr1, arr2);
+    }
+    public static double median(int[] arr1, int[] arr2)
+    {
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+        int left = (n1 + n2 + 1)/2;
+        int low = 0;
+        int high = n1;
+         while(low <= high)
+        {
+            int mid1 = low + (high - low)/2;
+            int mid2 = left - mid1;
+            int l1 = Integer.MIN_VALUE;
+            int l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE;
+            int r2 = Integer.MAX_VALUE;
+            if(mid1 < n1)
+                r1 = arr1[mid1];
+            if(mid2 < n2)
+                r2 = arr2[mid2];
+            if(mid1 >= 1)
+                l1 = arr1[mid1 - 1];
+            if(mid2 >= 1)
+                l2 = arr2[mid2 - 1];
+            if(l1<=r2 && l2<=r1)
+            {
+                if((n1+n2)%2 == 1)
+                    return Math.max(l1, l2);
+                return ((double)(Math.max(l1, l2) + Math.min(r1, r2)))/2.0;
+            }
+            else if(l1 > r2)
+                high = mid1 - 1;
+            else if(l2 > r1)
+                low = mid1 + 1;
+        }
+        return 0;
     }
 }
