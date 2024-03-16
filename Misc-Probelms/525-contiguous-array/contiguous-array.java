8c708ccd94bb3386f89e399d5b3e91c2258b6566
Siddharth1605 <110531782+Siddharth1605@users.noreply.github.com>
2024-03-16 20:29:02 +0530

    Time: 33 ms (5.92%) | Memory: 55.3 MB (30.88%) - LeetSync

---
+++ Misc-Probelms/525-contiguous-array/contiguous-array.java
@@ -0,0 +1,40 @@
+class Solution {
+    public static int getLongestSubarray(int []a, int k) {
+        int n = a.length; // size of the array.
+
+        Map<Integer, Integer> preSumMap = new HashMap<>();
+        int sum = 0;
+        int maxLen = 0;
+        for (int i = 0; i < n; i++) {
+            //calculate the prefix sum till index i:
+            sum += a[i];
+
+            // if the sum = k, update the maxLen:
+            if (sum == k) {
+                maxLen = Math.max(maxLen, i + 1);
+            }
+
+            // calculate the sum of remaining part i.e. x-k:
+            int rem = sum - k;
+
+            //Calculate the length and update maxLen:
+            if (preSumMap.containsKey(rem)) {
+                int len = i - preSumMap.get(rem);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            //Finally, update the map checking the conditions:
+            if (!preSumMap.containsKey(sum)) {
+                preSumMap.put(sum, i);
+            }
+        }
+
+        return maxLen;
+    }
+    public int findMaxLength(int[] arr) {
+        for(int i=0;i<arr.length;i++)
+            if(arr[i] == 0)
+                arr[i] = -1;
+        return getLongestSubarray(arr, 0);
+    }
+}
