16159f880ab765c3986cfe87a8af12f6e7b990c9
Siddharth1605 <110531782+Siddharth1605@users.noreply.github.com>
2026-02-18 08:51:55 +0530

    Updated with bfs solution

--- Graphs/NumberOfDistinctIslands_DFS.java
+++ Graphs/NumberOfDistinctIslands_DFS.java
@@ -1,6 +1,30 @@
 
 class Solution {
-    
+     void bfs(int pi, int pj, int[][] grid, int[][] vis, ArrayList<String> list)  {
+        
+        vis[pi][pj] = 1;
+        list.add(pi - pi + "_" + (pj - pj));
+        
+        int[] di = {0, -1, 0, 1};
+        int[] dj = {-1, 0, 1, 0};
+        
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(pi, pj));
+        
+        while(!queue.isEmpty()) {
+            Pair top = queue.poll();
+            for(int k=0;k<4;k++) {
+                int ci = di[k] + top.i;
+                int cj = dj[k] + top.j;
+                if(ci<0 || cj<0 || ci>=grid.length || cj>=grid[0].length || grid[ci][cj] == 0 ||
+            vis[ci][cj] == 1)
+                continue;
+                vis[ci][cj] = 1;
+                queue.add(new Pair(ci, cj));
+                list.add(ci - pi + "_" + (cj - pj));
+            }
+        }
+    }
     void dfs(int i, int j, int pi, int pj, int[][] grid, int[][] vis, ArrayList<String> list)  {
         if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j] == 0 ||
             vis[i][j] == 1)
