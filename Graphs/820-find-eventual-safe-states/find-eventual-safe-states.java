64ba3e9c9acdae98c7198a864e273ead0c6dc00f
Siddharth1605 <110531782+Siddharth1605@users.noreply.github.com>
2026-02-21 16:13:37 +0530

    Time: 4 ms (99.60%) | Memory: 65.9 MB (6.84%) - LeetSync

--- Graphs/820-find-eventual-safe-states/find-eventual-safe-states.java
+++ Graphs/820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -3,27 +3,31 @@ class Solution {
         int v = graph.length;
         int[] vis = new int[v];
         int[] path = new int[v];
+        int[] check = new int[v];
         List<Integer> ans = new ArrayList<>();
         for(int i=0;i<v;i++) {
             if(vis[i] == 0) {
-                dfs(i, vis, path, graph, ans);
+                dfs(i, vis, path, graph, check);
             }
         }
-        Collections.sort(ans);
+        for(int i=0;i<v;i++) {
+            if(check[i] == 1)
+                ans.add(i);
+        }
         return ans;
     }
 
-    public boolean dfs(int node, int[] vis, int[] path, int[][] graph, List<Integer> ans) {
+    public boolean dfs(int node, int[] vis, int[] path, int[][] graph, int[] check) {
         vis[node] = 1;
         path[node] = 1;
         for(int it : graph[node]) {
             if(path[it] == 1)
                 return false;
-            if(vis[it] == 0 && dfs(it, vis, path, graph, ans) == false)
+            if(vis[it] == 0 && dfs(it, vis, path, graph, check) == false)
                 return false;
         }
         path[node] = 0;
-        ans.add(node);
+        check[node] = 1;
         return true;
     }
 }
