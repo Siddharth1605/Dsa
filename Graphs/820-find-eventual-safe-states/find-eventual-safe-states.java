ef1d498aadc0cab1bb8f0b1f839fa5059fc3cbee
Siddharth1605 <110531782+Siddharth1605@users.noreply.github.com>
2026-02-23 09:09:35 +0530

    Time: 29 ms (19.98%) | Memory: 61.9 MB (55.94%) - LeetSync

--- Graphs/820-find-eventual-safe-states/find-eventual-safe-states.java
+++ Graphs/820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,6 +1,42 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int v = graph.length;
+        int[] indegree = new int[v];
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<v;i++)
+            adj.add(new ArrayList<>());
+        for(int i = 0; i < v; i++) {
+            for(int neighbor : graph[i]) {
+                adj.get(neighbor).add(i);
+                indegree[i]++;   
+            }
+        }
+        return bfs(indegree, adj);
+    }
+    public List<Integer> bfs(int[] indegree, List<List<Integer>> adj) {
+        List<Integer> ans = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i=0;i<indegree.length;i++) {
+            if(indegree[i] == 0)
+                queue.add(i);
+        }
+
+        while(!queue.isEmpty()) {
+            int top = queue.poll();
+            ans.add(top);
+            for(int it : adj.get(top)) {
+                indegree[it]--;
+                if(indegree[it] == 0)
+                    queue.add(it);
+            }
+        }
+
+        Collections.sort(ans);
+        return ans;
+    }
+    public List<Integer> eventualSafeNodesDFS(int[][] graph) {
+        int v = graph.length;
         int[] vis = new int[v];
         int[] path = new int[v];
         int[] check = new int[v];
